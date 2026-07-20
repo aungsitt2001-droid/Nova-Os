d22a543f30ef888c7a9e40fdc6ffd02f67277a87
aungsitt2001-droid <aungsitt2001@gmail.com>
2026-07-20 21:30:02 +0700

    Update HomeScreen.cpp

--- Screens/HomeScreen.cpp
+++ Screens/HomeScreen.cpp
@@ -1,9 +1,15 @@
 #include "HomeScreen.h"
+#include "../LGFX/LGFX_TDisplayS3.h"
 
 HomeScreen homeScreen;
 
 void HomeScreen::begin()
 {
+    lcd.fillScreen(TFT_BLACK);
+
+    lcd.setTextColor(TFT_WHITE);
+    lcd.setTextSize(2);
+    lcd.drawString("Home", 20, 20);
 }
 
 void HomeScreen::update()
