515f9446e57959bf02e9abac864433b43a337115
aungsitt2001-droid <aungsitt2001@gmail.com>
2026-07-20 21:23:37 +0700

    Update BootScreen.cpp

--- Screens/BootScreen.cpp
+++ Screens/BootScreen.cpp
@@ -1,9 +1,19 @@
 #include "BootScreen.h"
+#include "../LGFX/LGFX_TDisplayS3.h"
+#include "../Assets/BootLogo.h"
 
 BootScreen bootScreen;
 
 void BootScreen::begin()
 {
+    lcd.fillScreen(TFT_BLACK);
+
+    lcd.setTextColor(TFT_WHITE);
+    lcd.setTextSize(2);
+    lcd.drawString(BOOT_TEXT, 20, 120);
+
+    lcd.setTextSize(1);
+    lcd.drawString(BOOT_SUBTEXT, 20, 150);
 }
 
 void BootScreen::update()
