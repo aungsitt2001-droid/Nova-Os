10418272915922bc4cd7539711bb46a4e98998fe
aungsitt2001-droid <aungsitt2001@gmail.com>
2026-07-20 21:27:12 +0700

    Update ScreenManager.cpp

--- Core/ScreenManager.cpp
+++ Core/ScreenManager.cpp
@@ -1,11 +1,14 @@
 #include "ScreenManager.h"
+#include "../Screens/BootScreen.h"
 
 ScreenManager screenManager;
 
 void ScreenManager::begin()
 {
+    bootScreen.begin();
 }
 
 void ScreenManager::update()
 {
+    bootScreen.update();
 }
