5428148a940a12b9584e29a2f705bed555ca35cb
aungsitt2001-droid <aungsitt2001@gmail.com>
2026-07-20 21:29:13 +0700

    Update ScreenManager.cpp

--- Core/ScreenManager.cpp
+++ Core/ScreenManager.cpp
@@ -1,14 +1,46 @@
 #include "ScreenManager.h"
 #include "../Screens/BootScreen.h"
+#include "../Screens/HomeScreen.h"
 
 ScreenManager screenManager;
 
 void ScreenManager::begin()
 {
-    bootScreen.begin();
+    setScreen(ScreenType::Boot);
 }
 
 void ScreenManager::update()
 {
-    bootScreen.update();
+    switch (currentScreen)
+    {
+    case ScreenType::Boot:
+        bootScreen.update();
+        break;
+
+    case ScreenType::Home:
+        homeScreen.update();
+        break;
+
+    default:
+        break;
+    }
+}
+
+void ScreenManager::setScreen(ScreenType screen)
+{
+    currentScreen = screen;
+
+    switch (currentScreen)
+    {
+    case ScreenType::Boot:
+        bootScreen.begin();
+        break;
+
+    case ScreenType::Home:
+        homeScreen.begin();
+        break;
+
+    default:
+        break;
+    }
 }
