0a7131360d2b0e1a69f4563dc195406b38e49fb8
aungsitt2001-droid <aungsitt2001@gmail.com>
2026-07-20 21:28:16 +0700

    Update ScreenManager.h

--- Core/ScreenManager.h
+++ Core/ScreenManager.h
@@ -1,10 +1,23 @@
 #pragma once
 
+enum class ScreenType
+{
+    Boot,
+    Home,
+    Reader,
+    Settings
+};
+
 class ScreenManager
 {
 public:
     void begin();
     void update();
+
+    void setScreen(ScreenType screen);
+
+private:
+    ScreenType currentScreen = ScreenType::Boot;
 };
 
 extern ScreenManager screenManager;
